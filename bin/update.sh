984fbd1b4b68a3febec3366115870b27c15135f6
dangchienhsgs <dangchienhsgs@gmail.com>
2016-07-31 15:46:15 +0700

    Remove bin

--- bin/update.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-rsync -avzP --delete -e "ssh -p 2395" /mnt/Data/Projects/redis-protocol/redis-server/build/libs/redis-server-1.0.jar  chiennd@10.3.14.156:/home/chiennd/23.101/atp-service
